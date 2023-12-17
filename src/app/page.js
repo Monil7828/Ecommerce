24e0a0c9751687e49379ea22547ff8698e5b285b
Monil7828 <monilc2801@gmail.com>
2023-12-17 23:10:02 +0530

    Implementing common dialog component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="flex bg-slate-100 min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex bg-slate-200 min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-black">E commerce website</h1>
     </main>
   )
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,9 @@
 "use client";
 
+import { GlobalContext } from "@/context";
 import { adminNavOptions, navOptions, styles } from "@/utils";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import CommonModal from "../CommonModal";
 
 const isAdminView = false;
 const isAuthUser = true;
@@ -9,13 +11,15 @@ const user = {
   role: "admin",
 };
 
-function NavItems() {
+function NavItems({ isModalView = false }) {
   return (
     <div
-      className="items-center justify-between w-full md:flex md:w-auto"
+      className={`items-center justify-between w-full md:flex md:w-auto ${
+        isModalView ? "" : "hidden"
+      }`}
       id="nav-items"
     >
-      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
+      <ul className={`flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 ${isModalView ? "border-none" : "border border-gray-100"}`}>
         {isAdminView
           ? adminNavOptions.map((item) => (
               <li
@@ -39,9 +43,11 @@ function NavItems() {
 }
 
 export default function Navbar() {
+  const { showNavModal, setShowNavModal } = useContext(GlobalContext);
+
   return (
     <>
-      <nav className="bg-slate-300 shadow-sm fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+      <nav className="bg-slate-400 shadow-md fixed w-full z-20 top-0 left-0 border-b border-gray-200">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 text-black">
           <div className="flex  items-center cursor-pointer">
             <span className="self-center text-3xl font-bold underline text-slate-700 whitespace-nowrap">
@@ -51,7 +57,9 @@ export default function Navbar() {
           <div className="flex md:order-2 gap-2">
             {!isAdminView && isAuthUser ? (
               <Fragment>
-                <button className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white hover:bg-gray-700">Account</button>
+                <button className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white hover:bg-gray-700">
+                  Account
+                </button>
                 <button className={styles.button}>Cart</button>
               </Fragment>
             ) : null}
@@ -73,12 +81,13 @@ export default function Navbar() {
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-sticky"
               aria-expanded="false"
+              onClick={() => setShowNavModal(true)}
             >
               <span className="sr-only">Open main menu</span>
               <svg
                 className="w-6 h-6"
                 aria-hidden="true"
-                fill="currentColor"
+                fill="white"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -90,9 +99,15 @@ export default function Navbar() {
               </svg>
             </button>
           </div>
-          <NavItems />
+          <NavItems isModal={false} />
         </div>
       </nav>
+      <CommonModal
+        showModalTitle={false}
+        mainContent={<NavItems isModalView={true} />}
+        show={showNavModal}
+        setShow={setShowNavModal}
+      />
     </>
   );
 }
