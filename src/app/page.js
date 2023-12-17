2e3bc6e749d6dce4830df58ba04ad56dc64a296c
Monil7828 <monilc2801@gmail.com>
2023-12-17 16:44:01 +0530

    navbar component

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>E commerce website</h1>
+    <main className="flex bg-slate-100 min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-black">E commerce website</h1>
     </main>
   )
 }
