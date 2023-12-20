7480b914cf29c509b585433c1886c11226be8662
Monil7828 <monilc2801@gmail.com>
2023-12-20 16:54:04 +0530

    login page UI

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -11,9 +11,9 @@ export default function Register() {
     <div className="relative">
       <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-8 mr-auto xl:px-5 lg:flex-row">
         <div className="flex flex-col justify-center items-center w-full pr-10 pl-10 lg:flex-row ">
-          <div className="w-fullmt-20 mr-0 mb-0 ml-0 relative max-w-2xl lg:mt-0 lg:w-5/12 ">
+          <div className="w-full mt-20 mr-0 mb-0 ml-0 relative max-w-2xl lg:mt-0 lg:w-5/12 ">
             <div className="flex flex-col items-center justify-start pt-10 pr-10 pb-10 pl-10 bg-slate-300 shadow-2xl rounded-xl relative z-10">
-              <p className="w-full text-4xl font-medium text-center text-gray-600 font-serif">
+              <p className="w-full mb-3 text-4xl font-medium text-center text-gray-600 font-serif">
                 {isRegistered
                   ? "Registration Successfull !"
                   : "Sign up for an account"}
